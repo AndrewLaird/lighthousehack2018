4088e007b6b65fc2e54fafa5082f63cd18165a50
Andrew Laird <alaird@uci.edu>
2018-02-03 14:32:17 -0800

    hoping just adding one model won't break it

--- lighthousedjango/admin.py
+++ lighthousedjango/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 
-from lighthousedjango.models import Event#,Calendar,User
+from lighthousedjango.models import Event,User#,Calendar,User
 
 
 admin.site.register(Event)
 #admin.site.register(Calendar)
-#admin.site.register(User)
+admin.site.register(User)
--- lighthousedjango/models.py
+++ lighthousedjango/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 #
-# class User(models.Model):
-#     first  = models.CharField(max_length=75)
-#     last = models.CharField(max_length=75)
-#     #hashed with sha1 and hash lib
-#     hashed_password = models.CharField(max_length=500)
-#
+class User(models.Model):
+    first  = models.CharField(max_length=75)
+    last = models.CharField(max_length=75)
+    #hashed with sha1 and hash lib
+    hashed_password = models.CharField(max_length=500)
+    blocked_websites = models.CharField(max_length=1000)
+    totals = models.CharField(max_length=1000)
 # class Calendar(models.Model):
 #     name =models.CharField(max_length=500)
 #     user = models.ForeignKey('User',on_delete=models.PROTECT)
@@ -17,4 +18,5 @@ class Event(models.Model):
     title = models.CharField(max_length=500)
     description = models.CharField(max_length=1000)
     #calendar = models.ForeignKey('Calendar',on_delete=models.PROTECT)
+
 
