e58ef0da4119b54d57f266089b17e358e57e9d80
Andrew Laird <alaird@uci.edu>
2018-02-03 00:59:45 -0800

    words

---
+++ lighthousedjango/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+
+class Event(models.Model):
+    start = models.DateTimeField()
+    end = models.DateTimeField()
+    title = models.CharField(500)
+    description = models.CharField(1000)
+
--- lighthousedjango/views.py
+++ lighthousedjango/views.py
@@ -1,4 +1,7 @@
+from lighthousehack2018.lighthousedjango.models import Event
 from django.http import HttpResponse, JsonResponse
+from . import models
+
 
 def success(request):
 	return HttpResponse("that good succcc")
