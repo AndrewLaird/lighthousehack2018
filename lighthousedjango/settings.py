2916d93b0e0a92827069d188431f19e4cfb86e85
Andrew Laird <alaird@uci.edu>
2018-02-03 02:37:06 -0800

    adding project to installed apps

--- lighthousedjango/settings.py
+++ lighthousedjango/settings.py
@@ -29,7 +29,7 @@ DEBUG = True
 # Application definition
 
 INSTALLED_APPS = [
-    'lighthousedjango'
+    'lighthousedjango',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
