4793613eb48d3aac808745ec015638baef7c1ef7
Andrew Laird <alaird@uci.edu>
2018-02-03 11:16:38 -0800

    grossly involved

--- lighthousedjango/models.py
+++ lighthousedjango/models.py
@@ -1,9 +1,23 @@
 from django.db import models
-
+from django.contrib.postgres.fields import JSONField
 
 class Event(models.Model):
     start = models.DateTimeField()
     end = models.DateTimeField()
     title = models.CharField(max_length=500)
     description = models.CharField(max_length=1000)
-    
+    calendar = models.ForeignKey(Calendar)
+
+class Calendar(models.Model):
+    name =models.CharField(max_length=500)
+    user = models.ForeignKey(User)
+
+class User(models.Model):
+    first  = models.CharField(max_length=75)
+    last = models.CharField(max_length=75)
+    #hashed with sha1 and hash lib
+    hashed_password = models.CharField(max_length=500)
+    settings = models.ForeignKey(Settings)
+
+class Settings(models.Model):
+     black_list = JSONField()
