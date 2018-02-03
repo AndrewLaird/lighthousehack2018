c8fee3fe746b03681b3e3c2dcc37d908abc9214f
Andrew Laird <alaird@uci.edu>
2018-02-03 15:56:11 -0800

    password authentication

--- lighthousedjango/views.py
+++ lighthousedjango/views.py
@@ -27,7 +27,7 @@ def sign_in(request):
 	if(request.method == "POST"):
 		username =request.POST["username"]
 		password = request.POST["password"]
-		user = models.User.objects.fiter(username=username,password=password)
+		user = models.User.objects.filter(username=username,password=password)
 		if(user == None):
 			return HttpResponse(-1)
 		else:
