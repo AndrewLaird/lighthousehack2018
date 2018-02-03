e3475a4703523920592111f9ff8bce83a4a3c919
Andrew Laird <alaird@uci.edu>
2018-02-03 02:21:37 -0800

    removing files

--- lighthousedjango/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-
-class Event(models.Model):
-    start = models.DateTimeField()
-    end = models.DateTimeField()
-    title = models.CharField(max_length=500)
-    description = models.CharField(max_length=1000)
-    
