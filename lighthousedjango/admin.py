a281fbc33fa642c8bf151e0e71ef09f25a6631c6
Andrew Laird <alaird@uci.edu>
2018-02-03 11:28:14 -0800

    admin page

--- lighthousedjango/admin.py
+++ lighthousedjango/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 
-from lighthousedjango.models import Event
+from lighthousedjango.models import Event,Calendar,User,Settings
 
 admin.site.register(Event)
+admin.site.register(Calendar)
+admin.site.register(User)
+admin.site.register(Settings)
