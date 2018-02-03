73206b6061effb1256b78817a9d0be3b18e7be19
Andrew Laird <alaird@uci.edu>
2018-02-03 02:01:56 -0800

    whut

--- lighthousedjango/models.py
+++ lighthousedjango/models.py
@@ -4,7 +4,6 @@ from django.db import models
 class Event(models.Model):
     start = models.DateTimeField()
     end = models.DateTimeField()
-    title = models.CharField(500)
-    description = models.CharField(1000)
-
+    title = models.CharField(max_length=500)
+    description = models.CharField(max_length=1000)
     
