86b537d8d7938e47e3c1985572d36190e9855612
Andrew Laird <alaird@uci.edu>
2018-02-03 13:52:23 -0800

    moving app in settings file

--- lighthousedjango/settings.py
+++ lighthousedjango/settings.py
@@ -29,7 +29,6 @@ DEBUG = True
 # Application definition
 
 INSTALLED_APPS = [
-    'lighthousedjango',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -40,6 +39,8 @@ INSTALLED_APPS = [
     # http://whitenoise.evans.io/en/stable/django.html#using-whitenoise-in-development
     'whitenoise.runserver_nostatic',
     'django.contrib.staticfiles',
+    
+    'lighthousedjango',
 ]
 
 MIDDLEWARE = [
