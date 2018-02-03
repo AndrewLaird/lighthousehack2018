7a9accd2758b2f6203b51de506565b7eed39a13e
Andrew Laird <alaird@uci.edu>
2018-02-03 11:19:49 -0800

    models

--- lighthousedjango/models.py
+++ lighthousedjango/models.py
@@ -1,16 +1,9 @@
 from django.db import models
 from django.contrib.postgres.fields import JSONField
 
-class Event(models.Model):
-    start = models.DateTimeField()
-    end = models.DateTimeField()
-    title = models.CharField(max_length=500)
-    description = models.CharField(max_length=1000)
-    calendar = models.ForeignKey(Calendar)
 
-class Calendar(models.Model):
-    name =models.CharField(max_length=500)
-    user = models.ForeignKey(User)
+class Settings(models.Model):
+     black_list = JSONField()
 
 class User(models.Model):
     first  = models.CharField(max_length=75)
@@ -19,5 +12,16 @@ class User(models.Model):
     hashed_password = models.CharField(max_length=500)
     settings = models.ForeignKey(Settings)
 
-class Settings(models.Model):
-     black_list = JSONField()
+
+class Calendar(models.Model):
+    name =models.CharField(max_length=500)
+    user = models.ForeignKey(User)
+
+
+class Event(models.Model):
+    start = models.DateTimeField()
+    end = models.DateTimeField()
+    title = models.CharField(max_length=500)
+    description = models.CharField(max_length=1000)
+    calendar = models.ForeignKey(Calendar)
+
