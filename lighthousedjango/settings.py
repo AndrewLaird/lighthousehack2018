929e84af35a7e6fe0de4696f92dffc5adb7d2879
Andrew Laird <alaird@uci.edu>
2018-02-03 02:14:45 -0800

    maybe

--- lighthousedjango/settings.py
+++ lighthousedjango/settings.py
@@ -29,6 +29,7 @@ DEBUG = True
 # Application definition
 
 INSTALLED_APPS = [
+    "django.db",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- lighthousedjango/views.py
+++ lighthousedjango/views.py
@@ -1,6 +1,4 @@
 from django.http import HttpResponse, JsonResponse
-#from ..lighthousedjango import models
-
 
 def success(request):
 	return HttpResponse("that good succcc")
