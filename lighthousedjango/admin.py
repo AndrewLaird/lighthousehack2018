b9e701df223bd83e8e8ad72c4dd517ccd35d1a37
Andrew Laird <alaird@uci.edu>
2018-02-03 14:12:40 -0800

    removing migrate from procfile

--- lighthousedjango/admin.py
+++ lighthousedjango/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 
-from lighthousedjango.models import Event,Calendar,User
+from lighthousedjango.models import Event#,Calendar,User
 
 
 admin.site.register(Event)
-admin.site.register(Calendar)
-admin.site.register(User)
+#admin.site.register(Calendar)
+#admin.site.register(User)
