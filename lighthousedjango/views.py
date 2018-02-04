7718f8be56dd3bfc1d825f87c02a6c84ecc84d53
Andrew Laird <alaird@uci.edu>
2018-02-03 23:26:15 -0800

    sign up withou serializer

--- lighthousedjango/views.py
+++ lighthousedjango/views.py
@@ -64,13 +64,13 @@ def sign_up(request):
 			"blocked_websites":json_black_list,
 			"totals":json_totals,
 		}
-		new_user = UserSerializer(data=data)
-		new_user.run_validation(data=data)
-		if (new_user.is_valid()):
-			new_user.save()
-			return HttpResponse("Model created sucessfully", 200)
-		else:
-			return HttpResponse("Could not create model",400)
+		new_user = User.objects.create(data=data)
+		# new_user.run_validation(data=data)
+		# if (new_user.is_valid()):
+		# 	new_user.save()
+		return HttpResponse("Model created sucessfully", 200)
+		# else:
+		# 	return HttpResponse("Could not create model",400)
 	if (request.method == "GET"):
 		username = request.GET["username"]
 		password = request.GET["password"]
@@ -90,10 +90,7 @@ def sign_up(request):
 			"blocked_websites": json_black_list,
 			"totals": json_totals,
 		}
-		new_user = UserSerializer(data=data)
-		new_user.run_validation(data=data)
-		if (new_user.is_valid()):
-			new_user.save()
-			return HttpResponse("Model created sucessfully", 200)
-		else:
-			return HttpResponse("Could not create model", 400)
+		new_user = User.objects.create(data=data)
+		return HttpResponse("Model created sucessfully", 200)
+		# else:
+		# 	return HttpResponse("Could not create model", 400)
