979ddf2756d72b6058b7de42868adc100406a07a
Andrew Laird <alaird@uci.edu>
2018-02-04 02:16:53 -0800

    fixing database

--- lighthousedjango/views.py
+++ lighthousedjango/views.py
@@ -7,12 +7,6 @@ import json
 def success(request):
 	return HttpResponse("that good succcc")
 
-def cool_shit(request):
-	result_str = ""
-	for event in Event.objects.all():
-		result_str += "started: " + str(event.start) + " ended: " + str(event.end) + " and it was called " + str(event.title) +"<br />"
-	return HttpResponse(result_str)
-
 
 def log_activity(request):
 	if(request.method == "GET"):
